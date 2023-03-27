c8e8c104bd5b7bad6a64024840bfb803dc168e56
javier <223222@ids.upchiapas.edu.mx>
2023-03-27 13:51:20 -0600

    actualizaciones

--- src/main/java/com/example/proyectocorte3/HelloController.java
+++ src/main/java/com/example/proyectocorte3/HelloController.java
@@ -5,15 +5,31 @@ import javafx.scene.control.Label;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Random;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-    @FXML
+   
     private static HashMap<String,UtilesEscoleres>utilesEscolares=new HashMap<String,UtilesEscoleres>();
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    public String generarId(){
+        String codigo="";
+        int num=0;
+        Random random=new Random();
+        for (int i = 0; i < 4; i++) {
+            num+=random.nextInt(0,200);
+        }
+        codigo+=String.valueOf(num);
+        if (utilesEscolares.containsKey(codigo)){
+            codigo=generarId();
+        }
+        return codigo;
+
+    }
+
 }
